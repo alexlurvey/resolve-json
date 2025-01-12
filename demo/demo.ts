7a29659a70a5af1a2bd66b3d17894aa0a9041054
Alex Lurvey <alexlurvey@protonmail.com>
2025-01-11 21:36:56 -0600

    chore: add vite to run a a browser

---
+++ demo/demo.ts
@@ -0,0 +1,29 @@
+import { resolve } from '../src/resolve.ts';
+
+const config = {
+	descriptions: {
+		single: ['xf_pick', ['xf_hoist', '$value'], ['name']],
+		many: ['xf_map', '$value', ['xf_join', " '", ['$', 'name'], "'"]],
+	},
+	description: ['@@descriptions', '@input_type'],
+	inputs: {
+		has: 'single',
+		has_not: 'single',
+		has_any: 'many',
+		has_none: 'multiselect',
+	},
+	input_type: ['@@inputs', '$condition'],
+};
+
+let resolved = resolve(config, {
+	condition: 'has_any',
+});
+
+console.log('__________________________________________');
+
+resolved = resolve(resolved, {
+	condition: 'has_any',
+	value: [{ name: 'Option 1' }, { name: 'Option 2' }],
+});
+
+console.log('resolved', resolved);
